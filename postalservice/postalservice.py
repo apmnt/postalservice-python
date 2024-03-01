57822f9d63e4effab78e354e3e219055e70b3ee3
apmnt <aapo.montin@student.oulu.fi>
2024-03-01 14:01:39 +0200

    Add httpx import and update method names

--- postalservice/postalservice.py
+++ postalservice/postalservice.py
@@ -1,14 +1,15 @@
 from abc import ABC, abstractmethod
+import httpx
 from postalservice.utils.search_utils import SearchParams
 
 class PostalService(ABC):
     @abstractmethod
-    async def fetch_data(self, message: str):
+    async def fetch_data(self, message: str) -> httpx.Response:
 
         pass
 
     @abstractmethod
-    def parse_data(self, data: str):
+    def parse_response(self, response: str):
 
         pass
 
