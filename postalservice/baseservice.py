1923ff110003efab26ba1dd1358b23fa66e99270
apmnt <aapo.montin@student.oulu.fi>
2024-03-03 22:21:30 +0200

    Add async method for parsing response in BaseService

--- postalservice/baseservice.py
+++ postalservice/baseservice.py
@@ -21,13 +21,19 @@ class BaseService(ABC):
         pass
 
     @abstractmethod
+    async def parse_response_async(self, response: str) -> str:
+
+        pass
+
+
+    @abstractmethod
     def get_search_params(self, data: SearchParams) -> str:
 
         pass
 
     async def get_search_results_async(self, params: dict) -> str:
         res = await self.fetch_data_async(params)
-        return self.parse_response(res)
+        return await self.parse_response_async(res)
     
     def get_search_results(self, params: dict) -> str:
         res = self.fetch_data(params)
