b709108a296fcc5d81dd78e1224be1f1b425b4f2
apmnt <aapo.montin@student.oulu.fi>
2024-03-03 16:09:30 +0200

    Update setup.py with author and description

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='postalservice',
-    version='0.1.0',
+    name="postalservice",
+    version="0.1.0",
+    author="Aapo Montin",
+    description="A package for second hand shopping",
     packages=find_packages(),
+    long_description_content_type="text/markdown",
+    long_description=open("README.md").read(),
 )
