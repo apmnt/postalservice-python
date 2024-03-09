57b83540e90d314ab71fa6da2e266b6f71b48057
apmnt <aapo.montin@student.oulu.fi>
2024-03-09 12:16:58 +0200

    Refactor MercariService to use generate_payload_and_headers()

--- postalservice/mercariservice.py
+++ postalservice/mercariservice.py
@@ -17,7 +17,64 @@ SIZE_MAP = {
 }
 
 class MercariService(BaseService):
-    
+
+    def generate_payload_and_headers(self, params: dict):
+        if not isinstance(params, dict):
+            raise TypeError("params must be a dict")
+
+        keyword = params.get("keyword")
+        size = params.get("size")
+        item_count = params.get("item_count")
+
+        if size is not None:
+            mapped_size = SIZE_MAP.get(size)
+        else:
+            mapped_size = None
+
+        url = "https://api.mercari.jp/v2/entities:search"
+        searchSessionId = "".join(random.choice(CHARACTERS) for i in range(32))
+        payload = {
+            "userId": "",
+            "pageSize": item_count,
+            "searchSessionId": searchSessionId,
+            "indexRouting": "INDEX_ROUTING_UNSPECIFIED",
+            "thumbnailTypes": [],
+            "searchCondition": {
+                "keyword": keyword,
+                "excludeKeyword": "",
+                "sort": "SORT_CREATED_TIME",
+                "order": "ORDER_DESC",
+                "status": ["STATUS_ON_SALE"],
+                "sizeId": [mapped_size],
+                "categoryId": [],
+                "brandId": [],
+                "sellerId": [],
+                "priceMin": 0,
+                "priceMax": 0,
+                "itemConditionId": [],
+                "shippingPayerId": [],
+                "shippingFromArea": [],
+                "shippingMethod": [],
+                "colorId": [],
+                "hasCoupon": False,
+                "attributes": [],
+                "itemTypes": [],
+                "skuIds": [],
+            },
+            "defaultDatasets": ["DATASET_TYPE_MERCARI", "DATASET_TYPE_BEYOND"],
+            "serviceFrom": "suruga",
+            "userId": "",
+            "withItemBrand": True,
+            "withItemSize": True,
+        }
+        headers = {
+            "dpop": get_pop_jwt(url, "POST"),
+            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
+            "x-platform": "web",
+        }
+
+        return payload, headers
+
     async def fetch_data_async(self, params: dict) -> httpx.Response:
         """
         Fetches data from the Mercari API using the provided search parameters.
@@ -33,61 +90,13 @@ class MercariService(BaseService):
             Exception: If the API request fails.
         """
 
-        if not isinstance(params, dict):
-            raise TypeError('params must be a dict')
-
-        keyword = params.get('keyword')
-        size = params.get('size')
-        item_count = params.get('item_count')
-
-        if size is not None: mapped_size = SIZE_MAP.get(size)
-        else: mapped_size = None
-
         url = "https://api.mercari.jp/v2/entities:search"
-        searchSessionId = ''.join(random.choice(CHARACTERS) for i in range(32))
-        payload = {
-            "userId": "",
-            "pageSize": item_count,
-            "searchSessionId": searchSessionId,
-            "indexRouting": "INDEX_ROUTING_UNSPECIFIED",
-            "thumbnailTypes": [],
-            "searchCondition": {
-                "keyword": keyword,
-                "excludeKeyword": "",
-                "sort": "SORT_CREATED_TIME",
-                "order": "ORDER_DESC",
-                "status": ["STATUS_ON_SALE"],
-                "sizeId": [mapped_size],
-                "categoryId": [],
-                "brandId": [],
-                "sellerId": [],
-                "priceMin": 0,
-                "priceMax": 0,
-                "itemConditionId": [],
-                "shippingPayerId": [],
-                "shippingFromArea": [],
-                "shippingMethod": [],
-                "colorId": [],
-                "hasCoupon": False,
-                "attributes": [],
-                "itemTypes": [],
-                "skuIds": []
-            },
-            "defaultDatasets": ["DATASET_TYPE_MERCARI", "DATASET_TYPE_BEYOND"],
-            "serviceFrom": "suruga",
-            "userId": "",
-            "withItemBrand": True,
-            "withItemSize": True
-        }
-        headers = {
-            "dpop": get_pop_jwt(url, "POST"),
-            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
-            "x-platform": "web"
-        }
+        payload, headers = self.generate_payload_and_headers(params)
+
         async with httpx.AsyncClient() as client:
             response = await client.post(url, json=payload, headers=headers)
             return response
-            
+
     def fetch_data(self, params: dict) -> httpx.Response:
         """
         Fetches data from the Mercari API using the provided search parameters.
@@ -103,65 +112,13 @@ class MercariService(BaseService):
             Exception: If the API request fails.
         """
 
-        if not isinstance(params, dict):
-            raise TypeError('params must be a dict')
-
-        keyword = params.get('keyword')
-
-        size = params.get('size')
-        if size is not None: mapped_size = SIZE_MAP.get(size)
-        else: mapped_size = None
-
-        item_count = params.get('item_count')
-
         url = "https://api.mercari.jp/v2/entities:search"
-        searchSessionId = ''.join(random.choice(CHARACTERS) for i in range(32))
-        payload = {
-            "userId": "",
-            "pageSize": item_count,
-            "searchSessionId": searchSessionId,
-            "indexRouting": "INDEX_ROUTING_UNSPECIFIED",
-            "thumbnailTypes": [],
-            "searchCondition": {
-                "keyword": keyword,
-                "excludeKeyword": "",
-                "sort": "SORT_CREATED_TIME",
-                "order": "ORDER_DESC",
-                "status": ["STATUS_ON_SALE"],
-                "sizeId": [mapped_size],
-                "categoryId": [],
-                "brandId": [],
-                "sellerId": [],
-                "priceMin": 0,
-                "priceMax": 0,
-                "itemConditionId": [],
-                "shippingPayerId": [],
-                "shippingFromArea": [],
-                "shippingMethod": [],
-                "colorId": [],
-                "hasCoupon": False,
-                "attributes": [],
-                "itemTypes": [],
-                "skuIds": []
-            },
-            "defaultDatasets": ["DATASET_TYPE_MERCARI", "DATASET_TYPE_BEYOND"],
-            "serviceFrom": "suruga",
-            "userId": "",
-            "withItemBrand": True,
-            "withItemSize": True
-        }
-        headers = {
-            "dpop": get_pop_jwt(url, "POST"),
-            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
-            "x-platform": "web"
-        }
+        payload, headers = self.generate_payload_and_headers(params)
+
         with httpx.Client() as client:
             response = client.post(url, json=payload, headers=headers)
             return response
-            
 
-
-        
     def parse_response(self, response: httpx.Response) -> str:
         """
         Parses the response from the Mercari API and returns a JSON string of cleaned items.
@@ -180,27 +137,28 @@ class MercariService(BaseService):
         Returns:
             str: A JSON string of cleaned items.
         """
-    
-        items = json.loads(response.text)['items']
+
+        items = json.loads(response.text)["items"]
         cleaned_items_list = []
         for item in items:
             temp = {}
-            temp['id'] = item['id']
-            temp['title'] = item['name']
-            price = float(item.get('price'))
-            temp['price'] = price
-            if item.get('itemSize') is not None:
-                temp['size'] = item['itemSize'].get('name')
-            else: temp['size'] = None
-            temp['url'] =  'https://jp.mercari.com/item/' + item['id']
-            temp['img'] = item['thumbnails']
+            temp["id"] = item["id"]
+            temp["title"] = item["name"]
+            price = float(item.get("price"))
+            temp["price"] = price
+            if item.get("itemSize") is not None:
+                temp["size"] = item["itemSize"].get("name")
+            else:
+                temp["size"] = None
+            temp["url"] = "https://jp.mercari.com/item/" + item["id"]
+            temp["img"] = item["thumbnails"]
             cleaned_items_list.append(temp)
 
         item_json = json.dumps(cleaned_items_list)
         return item_json
-    
+
     async def parse_response_async(self, response: httpx.Response) -> str:
         return self.parse_response(response)
-    
+
     def get_search_params(self, data: SearchParams) -> str:
         return ""
