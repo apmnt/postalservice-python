73f618a200140882a99f2dcdd769cdf4e573bfa0
apmnt <aapo.montin@student.oulu.fi>
2024-03-28 21:52:18 -0500

    Update version number in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="postalservice",
-    version="0.1.2",
+    version="0.1.3",
     author="Aapo Montin",
     description="A package for second hand shopping",
     long_description=long_description,
